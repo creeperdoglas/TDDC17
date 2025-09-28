7fd670ef1b93c1cf3bf707311fc063a0436e0de1
creeperdoglas <borenmelkan@gmail.com>
2025-09-28 17:15:18 +0200

    lab 2 done

--- lab2/src/searchCustom/CustomBreadthFirstSearch.java
+++ lab2/src/searchCustom/CustomBreadthFirstSearch.java
@@ -1,11 +1,14 @@
 package searchCustom;
 
-import java.util.Random;
+//import java.util.Random;
 
-public class CustomBreadthFirstSearch  extends CustomGraphSearch{
+public class CustomBreadthFirstSearch extends CustomGraphSearch {
 
-	public   CustomBreadthFirstSearch(int maxDepth){
-		super(new Random().nextBoolean()); // Temporary random choice, you need to pick true or false!
-		System.out.println("Change line above in \"CustomBreadthFirstSearch.java\"!");
+	public CustomBreadthFirstSearch(int maxDepth) {
+		// super(new Random().nextBoolean()); // Temporary random choice, you need to
+		// pick true or false!
+		// System.out.println("Change line above in
+		// \"CustomBreadthFirstSearch.java\"!");
+		super(false); // BFS adds nodes to back of frontier (FIFO queue)
 	}
 };
